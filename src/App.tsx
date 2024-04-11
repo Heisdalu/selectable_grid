3593f63a3d3cd18a6e291716624ecbcd08c7b776
divine <divineobi07@gmail.com>
2024-04-11 12:45:05 +0100

    ref passage from Board to VirtualHighlight

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useRef } from "react";
 import "./App.css";
 import Board from "./views/Board/Board";
 import VirutalHighlight from "./views/VirutalHighlight/VirutalHighlight";
 
 function App() {
+  const boardRef = useRef<HTMLDivElement[]>([]);
+
   return (
     <div className="relative">
-      <VirutalHighlight />
-      <Board />
+      <VirutalHighlight boardRefs={boardRef} />
+      <Board ref={boardRef} />
       {/* <div className="lol">hello</div> */}
     </div>
   );
--- src/views/Board/Board.tsx
+++ src/views/Board/Board.tsx
@@ -1,8 +1,21 @@
-const Board = () => {
+import { forwardRef, useImperativeHandle, useRef, useCallback } from "react";
+
+const Board = forwardRef((_, ref) => {
+  const boardRef = useRef<HTMLDivElement[]>([]);
+
+  useImperativeHandle(ref, () => {
+    return boardRef;
+  });
+
+  const func = useCallback((ref: HTMLDivElement) => {
+    boardRef.current.push(ref);
+  }, []);
+
   const boardTiles = Array(100)
     .fill(1)
     .map((_, i) => (
       <div
+        ref={func}
         className="flex justify-center items-center h-[30px] border-1 w-[30px]"
         key={i}
       >
@@ -17,5 +30,5 @@ const Board = () => {
       </div>
     </div>
   );
-};
+});
 export default Board;
