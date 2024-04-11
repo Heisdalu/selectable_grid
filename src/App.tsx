e1422bc24fa15277ac82f150fc88edbac79bc569
divine <divineobi07@gmail.com>
2024-04-11 19:17:54 +0100

    add highlight to intecerpting boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useRef } from "react";
+import { useRef, MutableRefObject } from "react";
 import "./App.css";
 import Board from "./views/Board/Board";
 import VirutalHighlight from "./views/VirutalHighlight/VirutalHighlight";
 
+// { current: MutableRefObject<HTMLDivElement[]>
+interface Props {
+  current: MutableRefObject<HTMLDivElement[]>;
+}
+
 function App() {
-  const boardRef = useRef<HTMLDivElement[]>([]);
+  const boardRef = useRef({});
 
   return (
     <div className="relative">
-      <VirutalHighlight boardRefs={boardRef} />
+      <VirutalHighlight boardRefs={boardRef as Props} />
       <Board ref={boardRef} />
       {/* <div className="lol">hello</div> */}
     </div>
--- src/views/VirutalHighlight/VirutalHighlight.tsx
+++ src/views/VirutalHighlight/VirutalHighlight.tsx
@@ -1,6 +1,10 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, MutableRefObject } from "react";
 
-const VirutalHighlight = () => {
+const VirutalHighlight = ({
+  boardRefs,
+}: {
+  boardRefs: { current: MutableRefObject<HTMLDivElement[]> };
+}) => {
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [isMouseUp, setIsMouseUp] = useState(false);
   const virtualBoxRef = useRef<HTMLDivElement | null>(null);
@@ -17,29 +21,17 @@ const VirutalHighlight = () => {
 
   useEffect(() => {
     const mouseDownHandler = (e: MouseEvent) => {
-      // isMouseDown is true.. kindly reset virtual board
-
-      if (isMouseDown && virtualBoxRef.current) {
-        virtualBoxRef.current.style.transform = `translate(0, 0)`;
-        virtualBoxRef.current.style.height = "0";
-        virtualBoxRef.current.style.width = "0";
-
-        return setTimeout(() => {
-          setIsMouseUp(false);
-          setIsMouseDown(false);
-        }, 1000);
-      }
+      // reset highlight when mouse is down
 
       if (!virtualBoxRef.current || isMouseDown) return;
-      console.log(isMouseDown, "mouseDown");
+
+      boardRefs.current.current.forEach((item) => {
+        item.classList.remove("highlighted");
+      });
 
       setIsMouseDown(true);
-      //   setInitialPoint();
-      //   console.log(virtualBoxRef.current.getBoundingClientRect());
 
       virtualBoxRef.current.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-      //   console.log(virtualBoxRef.current.getBoundingClientRect());
-      //   console.log(virtualBoxRef.current.getBoundingClientRect());
 
       setInitialPoint(virtualBoxRef.current.getBoundingClientRect());
     };
@@ -47,19 +39,53 @@ const VirutalHighlight = () => {
     window.addEventListener("mousedown", mouseDownHandler);
 
     return () => window.removeEventListener("mousedown", mouseDownHandler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMouseDown]);
 
   useEffect(() => {
     const mouseUpHandler = () => {
       if (!virtualBoxRef.current || isMouseUp) return;
-      //   const rect = virtualBoxRef.current.getBoundingClientRect();
 
       setIsMouseUp(true);
+
+      const virtualRect = virtualBoxRef.current.getBoundingClientRect();
+
+      // subracted 25 from left and top and added 25 to right and bottom is to make them fit the box... sometimes user tends to draw a virtualBox from the middle of a box..adding and subract helps in making it equal so as to get a pefect intercept of boxes
+      const sumLeft = virtualRect.left - 25;
+      const sumRight = virtualRect.right + 25;
+      const sumTop = virtualRect.top - 25;
+      const sumBottom = virtualRect.bottom + 25;
+
+      // insert highlighted class to intercepting boxes on the board
+      boardRefs.current.current.forEach((item) => {
+        const rect_refs = item.getBoundingClientRect();
+
+        //check if it is intercepting
+        if (
+          rect_refs.left > sumLeft &&
+          sumRight > rect_refs.right &&
+          rect_refs.top > sumTop &&
+          sumBottom > rect_refs.bottom
+        ) {
+          item.classList.add("highlighted");
+        }
+      });
+
+      // reset virtual hightlight after highligthing
+      virtualBoxRef.current.style.transform = `translate(0, 0)`;
+      virtualBoxRef.current.style.height = "0";
+      virtualBoxRef.current.style.width = "0";
+
+      return setTimeout(() => {
+        setIsMouseUp(false);
+        setIsMouseDown(false);
+      }, 1000);
     };
 
     window.addEventListener("mouseup", mouseUpHandler);
 
     return () => window.removeEventListener("mouseup", mouseUpHandler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [initalPoint.left, initalPoint.top, isMouseUp]);
 
   useEffect(() => {
@@ -107,6 +133,7 @@ const VirutalHighlight = () => {
 
     window.addEventListener("mousemove", mouseMouseHandler);
     return () => window.removeEventListener("mousemove", mouseMouseHandler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMouseDown, isMouseUp, initalPoint]);
 
   return (
